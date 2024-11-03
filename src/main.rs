42b1c4cb76d534f49b67ddd7351ed4f5904379e2
Alex_Seiler <alexanderseiler00@gmail.com>
2024-11-03 15:00:12 -0500

    Error Handling
    Config::new is now Config::build and returns a Result in order to handle errors more appropriately

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::{env, fs};
+use std::{env, fs, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -19,9 +22,14 @@ struct Config {
     file_path: String,
 }
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments!");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
-        return Config {query, file_path}
+
+        return Ok(Config {query, file_path})
     }
 }
